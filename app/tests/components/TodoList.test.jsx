611570cf88a36e0dbc3b22c8036990f723b70751
Sam Okasha <Sam Okasha>
2016-08-12 01:11:30 +0400

    Fix jQuery require bug

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -3,7 +3,7 @@ var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
 var expect = require('expect');
 var TestUtils = require('react-addons-test-utils');
-var $ = require('jQuery');
+var $ = require('jquery');
 
 import {configure} from 'configureStore';
 // To require a export default use import.
