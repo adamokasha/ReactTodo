83ce57dd9a6159617478c9b974af8a97b7aa7cfa
Sam Okasha <Sam Okasha>
2016-08-13 17:51:35 +0400

    Add auth to action tests

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -34,24 +34,7 @@ describe('Actions', () => {
     expect(res).toEqual(action); 
   });
   
-  // done arg lets mocha know we're doing async test
-  // so it ends test when done is called 
-  it('should create todo and dispatch ADD_TODO', (done) => {
-    const store = createMockStore({});
-    const todoText = 'My todo item';
-    
-    store.dispatch(actions.startAddTodo(todoText)).then(() => {
-      // getActions returns array of all fns called on our mock store
-      const actions = store.getActions();
-      expect(actions[0]).toInclude({
-        type: 'ADD_TODO'
-      });
-      expect(actions[0].todo).toInclude({
-        text: todoText
-      });
-      done();
-    }).catch(done);
-  });
+
   
   it('should generate add todos action object', () => {
     var todos = [{
@@ -111,13 +94,20 @@ describe('Actions', () => {
   
   describe('Tests with firebase todos', () => {
     var testTodoRef;
+    var uid;
+    var todosRef;
     
     // mocha: allows us to set define some code to run before every test
     beforeEach((done) => {
-      var todosRef = firebaseRef.child('todos');
+      var credential = firebase.auth.GithubAuthProvider.credential(process.env.GITHUB_ACCESS_TOKEN);
       
-      todosRef.remove().then(() => {
-        testTodoRef = firebaseRef.child('todos').push();
+      firebase.auth().signInWithCredential(credential).then((user) => {
+        uid = user.uid;
+        todosRef = firebaseRef.child(`users/${uid}/todos`);
+        
+        return todosRef.remove();
+      }).then(() => {
+        testTodoRef = todosRef.push();
         
         return testTodoRef.set({
           text: 'Something to do',
@@ -132,11 +122,11 @@ describe('Actions', () => {
     
     // after
     afterEach((done) => {
-      testTodoRef.remove().then(() => done());
+      todosRef.remove().then(() => done());
     });
     
     it('should toggle todo and dipatch UPDATE_TODO action', (done) => {
-      const store = createMockStore({});
+      const store = createMockStore({auth: {uid}});
       const action = actions.startToggleTodo(testTodoRef.key, true);
       
       store.dispatch(action).then(() => {
@@ -158,7 +148,7 @@ describe('Actions', () => {
     });
     
     it('should populate todos and dispatch ADD_TODOS', (done) => {
-      const store = createMockStore({});
+      const store = createMockStore({auth: {uid}});
       const action = actions.startAddTodos();
       
       store.dispatch(action).then(() => {
@@ -171,6 +161,25 @@ describe('Actions', () => {
         
         done();
       }, done);
+    });
+    
+    // done arg lets mocha know we're doing async test
+    // so it ends test when done is called 
+    it('should create todo and dispatch ADD_TODO', (done) => {
+      const store = createMockStore({auth: {uid}});
+      const todoText = 'My todo item';
+      
+      store.dispatch(actions.startAddTodo(todoText)).then(() => {
+        // getActions returns array of all fns called on our mock store
+        const actions = store.getActions();
+        expect(actions[0]).toInclude({
+          type: 'ADD_TODO'
+        });
+        expect(actions[0].todo).toInclude({
+          text: todoText
+        });
+        done();
+      }).catch(done);
     });
   });
 });
